79d1541a5cdf8f003069ff07e51646e6f9c7c092
DGH <DGH@bab10101-e421-0410-a517-8ce0973de3ef>
2011-02-10 16:04:00 +0000

    Plugins SVN re-organization
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/cms/plugin-whatsnew@25920 bab10101-e421-0410-a517-8ce0973de3ef

---
+++ src/sql/plugins/whatsnew/core/create_whatsnew_portlet.sql
@@ -0,0 +1,14 @@
+--
+-- Structure for table whatsnew_portlet
+--
+
+DROP TABLE IF EXISTS whatsnew_portlet;
+CREATE TABLE whatsnew_portlet (
+  id_portlet int DEFAULT '0' NOT NULL,
+  show_documents smallint DEFAULT '0' NOT NULL,
+  show_portlets smallint DEFAULT '0' NOT NULL,
+  show_pages smallint DEFAULT '0' NOT NULL,
+  period int DEFAULT '0' NOT NULL,
+  nb_elements_max int DEFAULT '0' NOT NULL,
+  elements_order int DEFAULT '0' NOT NULL
+);
---
+++ src/sql/plugins/whatsnew/core/init_core_whatsnew_data_sample.sql
@@ -0,0 +1,5 @@
+--
+-- Dumping data for table whatsnew_portlet
+--
+INSERT INTO whatsnew_portlet (id_portlet,show_documents,show_portlets,show_pages,period,nb_elements_max,elements_order) VALUES 
+ (84,1,1,1,30,10,0);
